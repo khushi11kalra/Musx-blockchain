6e35a6ba1d80f6628e0c7bb0ce82d7d449e6486d
khushi <khushi11.kalra@gmail.com>
2023-07-05 02:20:07 +0530

    initial commit

---
+++ minor-grp51/src/Components/Colors.js
@@ -0,0 +1,10 @@
+export const COLORS = {
+    white: 'rgb(240,240,230)',
+    brown: '#FFACC7', //ffdd95
+    highlight: '#BCDAB4', //
+    black: 'rgb(4,16,13)', //)FEFCF3
+    blue :'#143642', //   00ABB3
+    blurWhite: 'rgb(255,255,255,0.75)',
+    blurBlack: 'rgb(0,0,0,0.75)', //
+
+  }
